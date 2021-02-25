6d5c034aae97bd59783c70d1f059c47192685adc
alcora <alcora@Alcora>
2021-02-25 19:10:43 +0200

    commit

--- Seminar 1/src/pachet1/Program.java
+++ Seminar 1/src/pachet1/Program.java
@@ -12,18 +12,18 @@ public class Program {
 		Girafa g1 = new Girafa("g1");
 		Girafa g2 = new Girafa("g2");
 		
-		zoo.adaugaAnimal(g1);
-		zoo.adaugaAnimal(g2);
+		zoo.addAnimal(g1);
+		zoo.addAnimal(g2);
 		
-		zoo.feedAnimals();
+		zoo.feedAllAnimals();
 		
 		Zebra z1 = new Zebra("z1");
 		Zebra z2 = new Zebra("z2");
 		
-		zoo.adaugaAnimal(z1);
-		zoo.adaugaAnimal(z2);
+		zoo.addAnimal(z1);
+		zoo.addAnimal(z2);
 		
-		zoo.feedAnimals();
+		zoo.feedAllAnimals();
 	}
 	
 }
