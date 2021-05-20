092a677249f86109e6ec672e6ed46e5c0874e317
alcora <alcora@Alcora>
2021-05-20 18:25:28 +0300

    Teste pentru clasa student

---
+++ Seminar12/src/ro/ase/cts/teste/StudentTests.java
@@ -0,0 +1,93 @@
+package ro.ase.cts.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ro.ase.cts.clase.Student;
+
+public class StudentTests {
+
+	@Test
+	public void testConstructorCuParametru() {
+		String nume ="Gigel";
+		Student student = new Student(nume);
+		
+		assertEquals(nume, student.getNume());
+		assertNotNull(student.getNote());
+	}
+
+	@Test
+	public void testConstructorFaraParametru() {
+		Student student = new Student();
+		
+		assertNotNull(student.getNote());
+		assertNotNull(student.getNume());
+	}
+	
+	@Test
+	public void testAdaugaNota() {
+		Student student = new Student();
+		
+		int nota = 5;
+		student.adaugaNota(nota);
+		assertEquals(nota, student.getNota(0));
+	}
+	
+	@Test
+	public void testAdaugaNotaDimensiune() {
+		Student student = new Student();
+		student.adaugaNota(5);
+		student.adaugaNota(8);
+		
+		assertEquals(2, student.getNote().size());
+	}
+	
+	@Test
+	public void testCalculeazaMedie() {
+		Student student = new Student();
+		student.adaugaNota(5);
+		student.adaugaNota(8);
+		
+		assertEquals(6.5f, student.calculeazaMedie(),0.1);
+	}
+	
+	
+	@Test
+	public void testCalculeazaMedieFaraNote() {
+		Student student = new Student();
+				
+		assertEquals(0, student.calculeazaMedie(),0.1);
+	}
+	
+	
+	@Test
+	public void testCalculeazaMedieCuONota() {
+		Student student = new Student();
+		student.adaugaNota(5);
+		
+		assertEquals(5, student.calculeazaMedie(),0.1);
+	}
+	
+	
+	@Test
+	public void testAreRestante() {
+		Student student = new Student();
+		student.adaugaNota(9);
+		student.adaugaNota(3);
+		
+		assertTrue(student.areRestante());
+	}
+	
+	
+	@Test
+	public void testNuRestante() {
+		Student student = new Student();
+		student.adaugaNota(9);
+		student.adaugaNota(8);
+		
+		assertFalse(student.areRestante());
+	}
+	
+	
+}
