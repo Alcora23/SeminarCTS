c7374f9c541316c2973625e8da3bcf1fb45111d6
alcora <alcora@Alcora>
2021-05-20 19:48:20 +0300

    Update

--- Seminar12/src/ro/ase/cts/teste/StudentTests.java
+++ Seminar12/src/ro/ase/cts/teste/StudentTests.java
@@ -21,8 +21,8 @@ public class StudentTests {
 	public void testConstructorFaraParametru() {
 		Student student = new Student();
 		
-		assertNotNull(student.getNote());
-		assertNotNull(student.getNume());
+		assertNotNull("Atributul note nu a fost initializat",student.getNote());
+		assertNotNull("Atributul nume nu a fost initializat",student.getNume());
 	}
 	
 	@Test
@@ -89,5 +89,52 @@ public class StudentTests {
 		assertFalse(student.areRestante());
 	}
 	
+	// varianta pentru unit3
+	@Test
+	public void testGetNotaExceptie() {
+		Student student = new Student();
+		
+		student.adaugaNota(9);
+		student.adaugaNota(8);
+		
+		try {
+			student.getNota(5);
+			fail("nu trebuie sa ajungem aici. Metoda nu arunca execptie");
+		}catch (IndexOutOfBoundsException e) {
+			
+		}catch(Exception e) {
+			fail("Metoda arunca o alta execptie");
+		}
+	}
 	
+	
+//	// varianta pentru unit4
+//		@Test( expected= IndexOutOfBoundsException.class)
+//		public void testGetNotaExceptieV4() {
+//			Student student = new Student();
+//			
+//			student.adaugaNota(9);
+//			student.adaugaNota(8);
+//			
+//			try {
+//				student.getNota(5);
+//				fail("nu trebuie sa ajungem aici. Metoda nu arunca execptie");
+//			}catch (IndexOutOfBoundsException e) {
+//				
+//			}catch(Exception e) {
+//				fail("Metoda arunca o alta execptie");
+//			}
+//		}
+		
+	// varianta pentru unit3
+		@Test
+		public void testGetNotaExceptieV5() {
+			Student student = new Student();
+			
+			student.adaugaNota(9);
+			student.adaugaNota(8);
+			
+			assertThrows(IndexOutOfBoundsException.class, () -> {student.getNota(5);});
+		}	
+
 }
